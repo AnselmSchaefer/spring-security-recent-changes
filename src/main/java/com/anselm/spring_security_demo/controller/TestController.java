43799066cd108b0dc28d02249d570243c58892b9
AnselmBenedikt <anselm.a.schaefer@basf.com>
2025-11-10 12:12:22 +0100

    access OIDC details

--- src/main/java/com/anselm/spring_security_demo/controller/TestController.java
+++ src/main/java/com/anselm/spring_security_demo/controller/TestController.java
@@ -1,12 +1,20 @@
 package com.anselm.spring_security_demo.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api")
 public class TestController {
+
+    private static final Log log = LogFactory.getLog(TestController.class);
 
     @GetMapping("/test")
     public String getHello() {
@@ -14,7 +22,16 @@ public class TestController {
     }
 
     @GetMapping("/private")
-    public String getPrivateHello() {
-        return "Hello (secret) World :)";
+    public String getPrivateHello(Authentication authentication) {
+        return "Hello " + this.getName(authentication);
     }
+
+    private String getName(Authentication authentication) {
+        return Optional.of(authentication.getPrincipal())
+                .filter(OidcUser.class::isInstance)
+                .map(OidcUser.class::cast)
+                .map(OidcUser::getEmail)
+                .orElseGet(authentication::getName);
+    }
+
 }
